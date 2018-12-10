0571c0836d1d9c0fabab44f3e10cee0da9fb0bb7
oswiliam <williamosmelmarquez1@gmail.com>
2018-12-10 14:09:10 -0600

    proyecto usando LINQ

---
+++ Ejemplo0212/Controllers/EstudiController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ejemplo0212.Models;
+
+namespace Ejemplo0212.Controllers
+{
+    public class EstudiController : Controller
+    {
+        Estudiantes db = new Estudiantes();
+        // GET: Estudi
+        public ActionResult Index()
+        {
+
+            var listado = db.Lista();
+            return View(listado.ToList());
+        }
+         [HttpPost]
+         public ActionResult Index(string txtbuscar,string select)
+         {
+            var query = from tbl in db.Lista() select tbl;
+           switch (select)
+           {
+                case "value1":
+                    var query1 = from p in db.Lista() where p.Materia.Contains(txtbuscar) select p;
+                 return View(query1.ToList());
+               case "value2":
+                 int a = Convert.ToInt32(txtbuscar);
+                   var query2 = from j in db.Lista() where j.Carnet == a select j;
+                   return View(query2.ToList());
+              case "value3":
+                  var query3 = from n in db.Lista() where n.Nombre.Contains(txtbuscar) select n;
+                  return View(query3.ToList());
+               case "value4":
+                   double e = Convert.ToDouble(txtbuscar);
+                   var query4 = from o in db.Lista() where o.Promedio == e select o;
+                    return View(query4.ToList());
+           }
+
+
+           return View(query.ToList());
+
+         }
+       
+           
+    }
+}
---
+++ Ejemplo0212/Models/Estudiantes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejemplo0212.Models
+{
+    public class Estudiantes
+    {
+        public int Carnet { get; set; }
+        public string Nombre { get; set; }
+        public string Materia { get; set; }
+        public double Promedio { get; set; }
+        public DateTime FechaRegistro { get; set; }
+
+
+        public List<Estudiantes> Lista()
+        {
+            var estudiantes1 = new Estudiantes()
+            {
+                Carnet = 001,
+                Nombre = "María",
+                Materia = "Literatura",
+                Promedio = 8.5,
+                FechaRegistro = DateTime.Now
+            };
+            var estudiantes2 = new Estudiantes()
+            {
+                Carnet = 002,
+                Nombre = "Juan",
+                Materia = "Gestión de BD",
+                Promedio = 3,
+                FechaRegistro = DateTime.Now
+            };
+            var estudiantes3 = new Estudiantes()
+            {
+                Carnet = 003,
+                Nombre = "Pedro",
+                Materia = "Linq",
+                Promedio = 8,
+                FechaRegistro = DateTime.Now
+            };
+            var estudiantes4 = new Estudiantes()
+            {
+                Carnet = 004,
+                Nombre = "Juan",
+                Materia = "Matemática",
+                Promedio = 7,
+                FechaRegistro = DateTime.Now
+            };
+            return new List<Estudiantes>() { estudiantes1, estudiantes2, estudiantes3, estudiantes4 };
+        }
+    }
+}
